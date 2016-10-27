d8f70f2905756e318b3d3eddc5e056494cb99e83
Chauncy <Chauncy523@gmail.com>
2016-10-27 19:12:16 +0800

    待完成：PTA乙级中文/1060. 爱丁顿数

--- C/PTA乙级中文/1060. 爱丁顿数.cpp
+++ C/PTA乙级中文/1060. 爱丁顿数.cpp
@@ -1,27 +1,34 @@
+/*
+WRONG !Wait for Check.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MaxSize 100001
 
-int cmp(const void*a,const void*b){
+int cmp(const void*a,const void*b) {
 	int x=*(int*)a;
 	int y=*(int*)b;
-	return x-y;
+	return y-x;
 }
 
-int main(){
-    int n;
+int main() {
+	int n;
 	scanf("%d",&n);
-int a[MaxSize];
-for(int i=0;i<n;i++)scanf("%d",&a[i]);
-qsort(a,n,sizeof(int),cmp);
+	int a[MaxSize];
+	for(int i=0; i<n; i++)scanf("%d",&a[i]);
+	qsort(a,n,sizeof(int),cmp);
 
-int i;
-for(i=n-1;i>0;i--){
-//	printf("{%d-%d}",a[i],i);
-	if(a[i]<n-i&&a[i+1]!=a[i])break;
-}
-printf("%d",a[i]);
+	int i;
+	for(i=0; a[i]>i&&i<n; i++) {
+//		printf("{%d-%d}",a[i],i);
+		
+	}
+	
+	if(i==n)printf("%d",n);
+	else if(a[i]==i) {
+		if(a[i-1]!=a[i])printf("%d",i);
+			else printf("%d",i-1);
+	} else	printf("%d",a[i]);
 	return 0;
 }
 
