065e0454d2cdefcb54bc02a486e519c18c6654e7
Chauncy <Chauncy523@gmail.com>
2016-10-27 18:13:33 +0800

    6.27：编写一个函数，它的参数是initializer_list<int>类型的对象，函数的功能室计算列表中所有元素的和

---
+++ C++/C++Primer5th/6_27initializer_list/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<initializer_list>
+
+using namespace std;
+
+auto Sum(initializer_list<int> k){
+    double sum=0;
+    for(auto i:k)sum+=i;
+    return sum;
+}
+
+int main(int argc, char *argv[])
+{
+
+    cout << Sum({1,2,3,4,5}) << endl;
+    return 0;
+}
