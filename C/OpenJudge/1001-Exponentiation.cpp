13f757c2572fa509dfdc6ab9d39fb67bd4b285a9
Chauncy <Chauncy523@gmail.com>
2016-11-15 11:53:52 +0800

    计算A+B

---
+++ C/OpenJudge/1001-Exponentiation.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*************************
+*功能：Calculate A+B
+*来自：OpenJudge
+*思路：缺省 
+*作者：Chauncy523@gmail.com
+*************************/
+
+
+int main(){
+    int a,b;
+	scanf("%d%d",&a,&b);
+printf("%d",a+b);
+
+	return 0;
+}
+
