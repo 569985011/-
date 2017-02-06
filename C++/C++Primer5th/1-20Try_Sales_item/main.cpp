3a4ac64155929d857b3b6af6d76b9b83da41419c
Chauncy <Chauncy523@gmail.com>
2017-02-06 12:53:51 +0800

    1.20 使用提供的Sales_item.h文件，读一组书籍销售数据并逐条输出

---
+++ C++/C++Primer5th/1-20Try_Sales_item/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include"Sales_item.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Sales_item book;
+    cin>>book;
+    cout<<book<<endl;
+    return 0;
+}
